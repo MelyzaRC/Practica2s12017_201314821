5905f80f237f3640c73ba3648b54d088956e2be6
MelyzaRC <mlzdrg792@gmail.com>
2017-02-27 18:39:52 -0600

    Creación de Proyecto

---
+++ practica2estructuras/Practica2Estructuras.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2estructuras;
+
+/**
+ *
+ * @author MelyzaR
+ */
+public class Practica2Estructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       MenuPrincipal m = new MenuPrincipal();
+       m.setVisible(true);
+    }
+    
+}
